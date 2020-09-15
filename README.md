1c9236b406e71c20aa282d9da30f299cbf8a0946
rada <you@example.com>
2020-09-15 16:58:46 +0000

    まだできてない

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # docker-mysql--
+
+
+
+
+docker-compose stop db2
+docker-compose rm -f db2
+docker-compose up -d db2
+docker-compose logs -f db2
--- initdb.d/aaa.sh
+++ initdb.d/aaa.sh
@@ -1,10 +1,16 @@
 pwd
 echo "docker-entrypoint-initdb.d"
 
-sleep 100
+sleep 50
 
 # db から db2 に データをコピー
 
+echo "mysqldump"
+mysqldump -h db -u root -pexample --databases mysql > /tmp/dump.sql
+cat /tmp/dump.sql
 
+echo "mysql import"
+# mysqld    -h db -u root -pexample --databases aa < /tmp/dump.sql
+mysqld -u root -pexample aaa < /tmp/dump.sql
 
-
+echo "docker-entrypoint-initdb.d"
